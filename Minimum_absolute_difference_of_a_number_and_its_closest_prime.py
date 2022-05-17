e0694448e962c003549d7efec710a011409efc31
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-05-17 12:54:53 +0530

    import math def p(n):     for i in range(2,int(n**0.5)+1):         if(n%i==0):             return 0     else:         return 1 n=int(input()) d2=0 d1=0 for i in range(n,10000000,+1):     if(p(i)):         d2=i         break for i in range(n,1,-1):     if(p(i)):         d1=i         break if(d2-n > n-d1):     print(abs(d1-n)) else:     print(abs(n-d2))

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,24 @@
+import math
+def p(n):
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):
+            return 0
+    else:
+        return 1
+n=int(input())
+d2=0
+d1=0
+for i in range(n,10000000,+1):
+    if(p(i)):
+        d2=i
+        break
+for i in range(n,1,-1):
+    if(p(i)):
+        d1=i
+        break
+if(d2-n > n-d1):
+    print(abs(d1-n))
+else:
+    print(abs(n-d2))
+    
+    
