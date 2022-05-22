fbd7829216f1cc54f2cb7ebc180a7f080e7cf526
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-05-22 19:29:15 +0530

    def p(n):     if(n==1):         return 1     for i in range(2,(n//2)+1):         if(n%i==0):             return 1     else:         return 0 n=int(input()) c=0 for i in range(1,n+1):     if(n%i==0):         if(p(i)):             c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,16 +1,15 @@
 def p(n):
+    if(n==1):
+        return 1
     for i in range(2,(n//2)+1):
         if(n%i==0):
-            return 0
+            return 1
     else:
-        return 1
+        return 0
 n=int(input())
 c=0
-c1=0
 for i in range(1,n+1):
     if(n%i==0):
         if(p(i)):
             c+=1
-        else:
-            c1+=1
-print(c1+1)
+print(c)
