43aba5ef3efa0aa901dcc812320d3e33a8568aa6
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-03 09:58:22 +0530

    n=input() a='aeiou' b=[] for i in n:     if i in a:         if i not in b:             b.append(i) if len(b)==5:     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,12 +1,11 @@
 n=input()
 a='aeiou'
-c=0
 b=[]
 for i in n:
     if i in a:
         if i not in b:
             b.append(i)
-if (len(b)==5):
+if len(b)==5:
     print(True)
 else:
     print(False)
