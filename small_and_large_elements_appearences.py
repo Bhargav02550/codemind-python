cbf942582a54ff1354fa7261730b9d7b07b9dc93
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-03 12:58:48 +0530

    n=input() a=' ' m=set(n) b=[] c,c1=0,0 for i in m:     if i not in a:         b.append(i) for i in n:     if min(b)==i:         c+=1 print(min(b),c,end=' ') for i in n:     if max(b)==i:         c1+=1 print(max(b),c1)

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,12 +1,16 @@
 n=input()
-n=n.split(" ")
-n=('').join(n)
-c=0
-d=0
+a=' '
+m=set(n)
+b=[]
+c,c1=0,0
+for i in m:
+    if i not in a:
+        b.append(i)
 for i in n:
-    if i==min(n):
+    if min(b)==i:
         c+=1
-    if i==max(n):
-        d+=1
-print(min(n),c,end=' ')
-print(max(n),d)
+print(min(b),c,end=' ')
+for i in n:
+    if max(b)==i:
+        c1+=1
+print(max(b),c1)
