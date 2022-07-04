b88fef83b3f4bd270c8e92e87fdc6df4985818eb
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-04 16:02:39 +0530

    s = input().lower().split(' ') m,c=0,0 for i in range(len(s)):     d = 0     for j in s[i]:         if j in 'aeiou':             d+=1     if d>m:         m=d for i in range(len(s)):     d = 0     for j in s[i]:         if j in 'aeiou':             d+=1     if d==m:         c+=1 print(c)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,17 @@
+s = input().lower().split(' ')
+m,c=0,0
+for i in range(len(s)):
+    d = 0
+    for j in s[i]:
+        if j in 'aeiou':
+            d+=1
+    if d>m:
+        m=d
+for i in range(len(s)):
+    d = 0
+    for j in s[i]:
+        if j in 'aeiou':
+            d+=1
+    if d==m:
+        c+=1
+print(c)
