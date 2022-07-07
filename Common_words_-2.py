7789490c9c880412a83ccef5f3f55751266a522e
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-07 15:50:52 +0530

    m=input().lower().split() n=input().lower().split() c=0 for i in n:     if i in m and m.count(i)==1 and n.count(i)==1:         c+=1 print(c)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -1,5 +1,5 @@
-n=input().lower().split(" ")
-m=input().lower().split(" ")
+m=input().lower().split()
+n=input().lower().split()
 c=0
 for i in n:
     if i in m and m.count(i)==1 and n.count(i)==1:
