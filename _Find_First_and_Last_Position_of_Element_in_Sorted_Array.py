340aaed127ad9754dca18882a6c40fec5b413685
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-09 20:57:00 +0530

    def findFirstAndLast(arr, n, x) :     first = -1     last = -1     for i in range(0, n) :         if (x != arr[i]) :             continue         if (first == -1) :             first = i         last = i
        if (first != -1) :
            print(first,last)
        else :
            print("-1 -1")
    n=int(input())
    a=list(map(int,input().split()))
    x =int(input())
    findFirstAndLast(a,n,x)

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,18 @@
+def findFirstAndLast(arr, n, x) :
+    first = -1
+    last = -1
+    for i in range(0, n) :
+        if (x != arr[i]) :
+            continue
+        if (first == -1) :
+            first = i
+        last = i
+     
+    if (first != -1) :
+        print(first,last)
+    else :
+        print("-1 -1")
+n=int(input())
+a=list(map(int,input().split()))
+x =int(input())
+findFirstAndLast(a,n,x)
