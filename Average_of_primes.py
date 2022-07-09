cf30609440a4f9dcc30f039604469b0f339f0f75
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-09 15:21:42 +0530

    def p(n):     if n==1:         return 0     for i in range(2,int(n/2)+1):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split())) c,s=0,0 for i in a:     if p(i):         s+=i         c+=1 k=s/c print("%.2f"%k)

---
+++ Average_of_primes.py
@@ -0,0 +1,16 @@
+def p(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n/2)+1):
+        if n%i==0:
+            return 0
+    return 1
+n=int(input())
+a=list(map(int,input().split()))
+c,s=0,0
+for i in a:
+    if p(i):
+        s+=i
+        c+=1
+k=s/c
+print("%.2f"%k)
