34eda8781c0a4465247f058df2c554a91496bf1d
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-09 15:28:19 +0530

    def p(n):     if n==1:         return 0     for i in range(2,int(n/2)+1):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split())) c=0 mi=a.index(min(a)) ma=a.index(max(a)) if mi<ma:     for i in range(mi,ma+1):         if p(a[i]):             c+=1     print(c) else:     for i in range(ma,mi+1):         if p(a[i]):             c+=1     print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -1,18 +1,22 @@
 def p(n):
     if n==1:
         return 0
-    for i in range(2,int((n**0.5)+1)):
+    for i in range(2,int(n/2)+1):
         if n%i==0:
             return 0
     return 1
 n=int(input())
 a=list(map(int,input().split()))
 c=0
-m=a.index(max(a))
-m1=a.index(min(a))
-if m<m1:
-    m,m1=m1,m
-for i in range(m1,m+1):
-    if p(a[i]):
-        c+=1
-print(c)
+mi=a.index(min(a))
+ma=a.index(max(a))
+if mi<ma:
+    for i in range(mi,ma+1):
+        if p(a[i]):
+            c+=1
+    print(c)
+else:
+    for i in range(ma,mi+1):
+        if p(a[i]):
+            c+=1
+    print(c)
