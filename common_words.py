33f4c41bcc1a59c2663cd3d1995d39c2f4028a4c
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-10 10:03:03 +0530

    n=input().lower().split() m=input().lower().split() for i in m:     if i in n:         print(i,end=' ')

--- common_words.py
+++ common_words.py
@@ -1,5 +1,5 @@
 n=input().lower().split()
 m=input().lower().split()
-for i in range(0,len(m)):
-    if m[i] in n:
-        print(m[i],end=' ')
+for i in m:
+    if i in n:
+        print(i,end=' ')
