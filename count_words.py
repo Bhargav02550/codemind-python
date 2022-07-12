4260a826ee6635cdc10e423266af146c46bf92e2
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-12 14:41:45 +0530

    n=input().lower().split(" ") c=0 for i in range(len(n)):     a = " ".join(n[i])     if a[0] in 'aeiou' and a[len(a)-1] in 'qwrtypsdfghjklzxcvbnm':         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,9 +1,7 @@
-n = input()
-n = n.lower()
-n = n.split(" ")
-count = 0
+n=input().lower().split(" ")
+c=0
 for i in range(len(n)):
     a = " ".join(n[i])
     if a[0] in 'aeiou' and a[len(a)-1] in 'qwrtypsdfghjklzxcvbnm':
-        count += 1
-print(count)
+        c+=1
+print(c)
