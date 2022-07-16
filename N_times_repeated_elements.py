a8f2398079e9a711c59c36e1299ce12cb452a0f4
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-07-16 14:47:46 +0530

    n=int(input()) a=list(map(int,input().split())) m=int(input()) c=0 for i in set(a):     if a.count(i)==m:         c+=1         print(i,end=' ') if c==0:     print("-1")

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,13 +1,10 @@
 n=int(input())
 a=list(map(int,input().split()))
 m=int(input())
-b=[]
 c=0
-for i in a:
+for i in set(a):
     if a.count(i)==m:
         c+=1
-        b.append(i)
-if c!=0:
-    print(*set(b))
-else:
+        print(i,end=' ')
+if c==0:
     print("-1")
