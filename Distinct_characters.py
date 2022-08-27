300de6d3b96e4f8dcf04f9761c0dc367dd056fff
Bhargav02550 <99163827+Bhargav02550@users.noreply.github.com>
2022-08-27 16:03:43 +0530

    n=input().lower() b=[] for i in n:     if i not in b and i!=' ':         b.append(i) for i in sorted(b):     print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,17 +1,7 @@
-n = input()
-n = n.lower()
-n = n.replace(" ","")
-s = []
-for i in range(0,len(n)):
-    c = n.count(n[i])
-    if c==1:
-        s.append(n[i])
-s.sort()
-s = str(s)
-s = s.replace(",","")
-s = s.replace("[","")
-s = s.replace("]","")
-s = s.replace(",","")
-s = s.replace("'","")
-s = s.replace(" ","")
-print(s)
+n=input().lower()
+b=[]
+for i in n:
+    if i not in b and i!=' ':
+        b.append(i)
+for i in sorted(b):
+    print(i,end='')
