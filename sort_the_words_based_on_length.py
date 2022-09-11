47cf16cb6fdfae5e1637258c9f9503ed38cfe6ee
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-09-11 17:24:03 +0530

    n = list(map(str,input().split())) for i in range(0,len(n)):     for j in range(0,len(n)):         if i!=j and len(n[i])<len(n[j]):             temp=n[i]             n[i]=n[j]             n[j]=temp print(*n)

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,8 +1,8 @@
-n=input().split(" ")
-for i in range(len(n)):
-    for j in range(len(n)):
+n = list(map(str,input().split()))
+for i in range(0,len(n)):
+    for j in range(0,len(n)):
         if i!=j and len(n[i])<len(n[j]):
-            temp=n[j]
-            n[j]=n[i]
-            n[i]=temp
+            temp=n[i]
+            n[i]=n[j]
+            n[j]=temp
 print(*n)
