2f58a714878f615f0ee8d532512ddf1a373a1810
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-10-07 12:13:32 +0530

    def toGoatLatin(S):         words = S.split()         vowels = set("AEIOUaeiou")         for i in range(len(words)):             if words[i][0] in vowels:                 words[i] = words[i] + "ma" + "a"*(i+1)             else:                 words[i] = words[i][1:] + words[i][0] + "ma" + "a"*(i+1)
            return " ".join(words)
    n=input()
    print(toGoatLatin(n))

---
+++ Goat_Latin.py
@@ -0,0 +1,12 @@
+def toGoatLatin(S):
+        words = S.split()
+        vowels = set("AEIOUaeiou")
+        for i in range(len(words)):
+            if words[i][0] in vowels:
+                words[i] = words[i] + "ma" + "a"*(i+1)
+            else:
+                words[i] = words[i][1:] + words[i][0] + "ma" + "a"*(i+1)
+                
+        return " ".join(words)
+n=input()
+print(toGoatLatin(n))
