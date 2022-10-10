3e32c2a493cbd00d37c6d333f3d4c4cf770e1aa1
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-10-10 21:41:12 +0530

    def romanToInt(s):     roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}     z = 0     for i in range(0, len(s) - 1):         if  roman[s[i]] < roman[s[i+1]]:             z -= roman[s[i]]         else:             z += roman[s[i]]     return z + roman[s[-1]] n=input() print(romanToInt(n))

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,36 +1,11 @@
-def value(r):
-	if (r == 'I'):
-		return 1
-	if (r == 'V'):
-		return 5
-	if (r == 'X'):
-		return 10
-	if (r == 'L'):
-		return 50
-	if (r == 'C'):
-		return 100
-	if (r == 'D'):
-		return 500
-	if (r == 'M'):
-		return 1000
-	return -1
-
-def romanToDecimal(str):
-	res = 0
-	i = 0
-	while (i < len(str)):
-		s1 = value(str[i])
-		if (i + 1 < len(str)):
-			s2 = value(str[i + 1])
-			if (s1 >= s2):
-				res = res + s1
-				i = i + 1
-			else:
-				res = res + s2 - s1
-				i = i + 2
-		else:
-			res = res + s1
-			i = i + 1
-	return res
+def romanToInt(s):
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+    z = 0
+    for i in range(0, len(s) - 1):
+        if  roman[s[i]] < roman[s[i+1]]:
+            z -= roman[s[i]]
+        else:
+            z += roman[s[i]]
+    return z + roman[s[-1]]
 n=input()
-print(romanToDecimal(n))
+print(romanToInt(n))
