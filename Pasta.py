261fe97b676749ffbaca4f33beb80895a9002e64
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-10-11 14:41:04 +0530

    n,m = map(int,input().split()) a = list(map(int,input().split())) b = list(map(int,input().split())) c = 0 se = set(b) if len(se)==len(b):     for i in b:         if i in a:             c+=1     if c==len(b):         print("Yes")     else:         print("No") else:     print("No")

---
+++ Pasta.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+c = 0
+se = set(b)
+if len(se)==len(b):
+    for i in b:
+        if i in a:
+            c+=1
+    if c==len(b):
+        print("Yes")
+    else:
+        print("No")
+else:
+    print("No")
