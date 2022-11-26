edfe50953f4f5820ceede75bf6c560a827ae0608
bhargav0255 <99163827+Bhargav02550@users.noreply.github.com>
2022-11-26 09:00:53 +0530

    for i in range(int(input())):     n=int(input())     a=list(map(int,input().split()))     for i in range(n):         if a.count(a[i])>1:             print(a[i])             break

---
+++ Find_Duplicate_.py
@@ -0,0 +1,7 @@
+for i in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    for i in range(n):
+        if a.count(a[i])>1:
+            print(a[i])
+            break
